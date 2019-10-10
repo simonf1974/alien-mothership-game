b95e902a2b199fc96810e7986927eb6d35082275
Simon Freedland <simon.freedland@nology.io>
2019-10-10 17:43:50 +0100

    created tests for killing only queen and killing all but queen

--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { MotherShip } from "./classes";
+import { MotherShip, Aliens } from "./classes";
 
 describe("These are my class tests", () => {
   let ship;
@@ -6,9 +6,111 @@ describe("These are my class tests", () => {
     ship = new MotherShip();
   });
 
-  test("Check initialisation of ship", () => {
+  const fire = (numOfFires, indexOfAlien, ship) => {
+    global.Math.random = () => indexOfAlien / ship.aliveAliens.length;
+    for (let i = 0; i < numOfFires; i++) {
+      ship.fire();
+    }
+  };
+
+  test("Check firing on Queen and killing her", () => {
     expect(ship.aliens.length).toEqual(14);
-    console.log(ship.aliens);
-    // expect(ship.getAliveAliens.length).toEqual(14);
+    expect(ship.aliveAliens.length).toEqual(14);
+
+    global.Math.random = () => 0.0714285714285714;
+    fire(1, 0, ship);
+    expect(ship.aliveAliens.length).toEqual(14);
+    expect(ship.aliens[0].currentHitPoints).toEqual(73);
+    fire(1, 0, ship);
+    expect(ship.aliveAliens.length).toEqual(14);
+    expect(ship.aliens[0].currentHitPoints).toEqual(66);
+    fire(8, 0, ship);
+    expect(ship.aliveAliens.length).toEqual(14);
+    expect(ship.aliens[0].currentHitPoints).toEqual(10);
+    fire(1, 0, ship);
+    expect(ship.aliveAliens.length).toEqual(14);
+    expect(ship.aliens[0].currentHitPoints).toEqual(3);
+    expect(ship.aliens[0].isAlive).toEqual(true);
+    expect(ship.isGameOver).toEqual(false);
+    fire(1, 0, ship);
+    expect(ship.aliveAliens.length).toEqual(13);
+    expect(ship.aliens[0].currentHitPoints).toEqual(0);
+    expect(ship.aliens[0].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(true);
+  });
+
+  test("Check firing on all aliens except queen", () => {
+    // worker0
+    expect(ship.aliveAliens.length).toEqual(14);
+    expect(ship.aliens[1].currentHitPoints).toEqual(68);
+    fire(6, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(14);
+    expect(ship.aliens[1].currentHitPoints).toEqual(8);
+    expect(ship.aliens[1].isAlive).toEqual(true);
+    expect(ship.isGameOver).toEqual(false);
+    fire(1, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(13);
+    expect(ship.aliens[1].currentHitPoints).toEqual(0);
+    expect(ship.aliens[1].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(false);
+
+    // worker1
+    fire(7, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(12);
+    expect(ship.aliens[2].currentHitPoints).toEqual(0);
+    expect(ship.aliens[2].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(false);
+
+    // worker2
+    fire(7, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(11);
+    expect(ship.aliens[3].currentHitPoints).toEqual(0);
+    expect(ship.aliens[3].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(false);
+
+    // worker3
+    fire(7, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(10);
+    expect(ship.aliens[4].currentHitPoints).toEqual(0);
+    expect(ship.aliens[4].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(false);
+
+    // worker4
+    fire(7, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(9);
+    expect(ship.aliens[5].currentHitPoints).toEqual(0);
+    expect(ship.aliens[5].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(false);
+
+    //drone0
+    expect(ship.aliens[6].currentHitPoints).toEqual(60);
+    fire(4, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(9);
+    expect(ship.aliens[6].currentHitPoints).toEqual(12);
+    expect(ship.aliens[6].isAlive).toEqual(true);
+    expect(ship.isGameOver).toEqual(false);
+    fire(1, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(8);
+    expect(ship.aliens[6].currentHitPoints).toEqual(0);
+    expect(ship.aliens[6].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(false);
+
+    fire(5, 1, ship);
+    fire(5, 1, ship);
+    fire(5, 1, ship);
+    fire(5, 1, ship);
+    fire(5, 1, ship);
+
+    fire(5, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(2);
+    expect(ship.aliens[12].currentHitPoints).toEqual(0);
+    expect(ship.aliens[12].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(false);
+
+    fire(5, 1, ship);
+    expect(ship.aliveAliens.length).toEqual(1);
+    expect(ship.aliens[13].currentHitPoints).toEqual(0);
+    expect(ship.aliens[13].isAlive).toEqual(false);
+    expect(ship.isGameOver).toEqual(false);
   });
 });
